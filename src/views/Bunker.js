f346b17f0f254b3ef7fb6669335a85c35357fc40
Franco <francogarcino@gmail.com>
2024-06-09 00:24:19 -0300

    fixes

--- src/views/Bunker.js
+++ src/views/Bunker.js
@@ -1,4 +1,4 @@
-import {useEffect, useState} from "react";
+import {useState} from "react";
 import api from "../api/api";
 import size from "../utils/utils";
 import {useNavigate} from "react-router-dom";
@@ -9,32 +9,19 @@ export default function Bunker() {
     const navigate = useNavigate();
     const goHome = () => { navigate("/") }
 
-    let uris = [
-        process.env.REACT_APP_URI_PANCHO || "",
-        process.env.REACT_APP_URI_ACIDC || "",
-        process.env.REACT_APP_URI_JYQ || "",
-        process.env.REACT_APP_URI_BASE || "",
-        process.env.REACT_APP_URI_TECH || "",
-        process.env.REACT_APP_URI_TEST || "",
-        process.env.REACT_APP_URI_MARIA || "",
-        process.env.REACT_APP_URI_ESTRA || ""
-    ]
-
     const [docs, setDocs] = useState([]);
+    const [msg, setMsg] = useState("Seleccione la terminal a consultar");
 
-    useEffect(() => {
-        for (let uri of uris) {
-            if (uri) {
-                api.androids(uri).then((response) => {
-                    let droids = response.data.map(doc => ({
-                        ...doc,
-                        origin: mapUri(uri)
-                    }));
-                    setDocs([...docs, ...droids])
-                }).catch(e => console.log(`El terminal ${uri} no responde...`));
-            }
-        }
-    }, [])
+    const fetchFrom = (uri) => {
+        setMsg(`Recuperando consciencias de ${mapUri(uri)[0]}`)
+        api.androids(uri).then((response) => {
+            let droids = response.data.map(doc => ({
+                ...doc,
+                origin: mapUri(uri)
+            }));
+            setDocs(droids)
+        }).catch(e => console.log(`El terminal ${uri} no responde...`));
+    }
 
     return (
         <div className="page">
@@ -42,14 +29,48 @@ export default function Bunker() {
                 <div className="text title"> 🧠 Conciencias r[e]gistradas</div>
                 <button className="back-btn text" onClick={goHome}> Volver al inicio</button>
             </div>
+            <div className="margined btn-container">
+                <button className="db-btn"
+                        disabled={!Boolean(process.env.REACT_APP_URI_PANCHO || "")}
+                        onClick={() => fetchFrom(process.env.REACT_APP_URI_PANCHO)}> EPERS
+                </button>
+                <button className="db-btn"
+                        disabled={!Boolean(process.env.REACT_APP_URI_ACIDC || "")}
+                        onClick={() => fetchFrom(process.env.REACT_APP_URI_ACIDC)}> ACIDC
+                </button>
+                <button className="db-btn"
+                        disabled={!Boolean(process.env.REACT_APP_URI_BASE || "")}
+                        onClick={() => fetchFrom(process.env.REACT_APP_URI_BASE)}> BASE DE GATOS
+                </button>
+                <button className="db-btn"
+                        disabled={!Boolean(process.env.REACT_APP_URI_ESTRA || "")}
+                        onClick={() => fetchFrom(process.env.REACT_APP_URI_ESTRA)}> ESTRATEGAS
+                </button>
+                <button className="db-btn"
+                        disabled={!Boolean(process.env.REACT_APP_URI_JYQ || "")}
+                        onClick={() => fetchFrom(process.env.REACT_APP_URI_JYQ)}> JAMON Y QUESO
+                </button>
+                <button className="db-btn"
+                        disabled={!Boolean(process.env.REACT_APP_URI_MARIA || "")}
+                        onClick={() => fetchFrom(process.env.REACT_APP_URI_MARIA)}> MARIADB
+                </button>
+                <button className="db-btn"
+                        disabled={!Boolean(process.env.REACT_APP_URI_TECH || "")}
+                        onClick={() => fetchFrom(process.env.REACT_APP_URI_TECH)}> TECH TROOPERS
+                </button>
+                <button className="db-btn"
+                        disabled={!Boolean(process.env.REACT_APP_URI_TEST || "")}
+                        onClick={() => fetchFrom(process.env.REACT_APP_URI_TEST)}> TEST APROB.
+                </button>
+            </div>
             {
                 docs.length > 0 ?
                     <div className="margined droids-container">
                         {docs.map((doc, index) => card(doc, index))}
                     </div>
-                        :
+                    :
                     <div className="loading-msg">
-                        💻 Recuperando conciencias...
+                        💻 {msg}
                     </div>
             }
         </div>
